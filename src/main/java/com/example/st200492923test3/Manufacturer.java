006cadf6190bacc23a29642dc024be0e50d46c35
sky1128 <pa.pruthvish@gmail.com>
2022-04-21 11:19:15 -0400

    Initial commit

---
+++ src/main/java/com/example/st200492923test3/Manufacturer.java
@@ -0,0 +1,8 @@
+package com.example.st200492923test3;
+
+public class Manufacturer {
+    private String name;
+    private String address;
+    private String phoneNumber;
+
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.st200492923test3 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.st200492923test3 to javafx.fxml;
+    exports com.example.st200492923test3;
+}
